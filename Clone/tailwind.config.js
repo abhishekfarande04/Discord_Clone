21db35ac61ded411b88c4a0a80f589f1948fa10f
abhishekfarande04 <farandeabhishek04@gmail.com>
2023-04-20 00:23:19 +0530

    update

--- Clone/tailwind.config.js
+++ Clone/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["*"],
   theme: {
     extend: {},
   },
   plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
 };
